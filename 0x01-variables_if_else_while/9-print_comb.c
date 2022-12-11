3b54164ec207cba0e61b4accbe9c91470ef11889
Sikiru Yaya <sikiruyaya1@gmail.com>
2022-12-11 14:04:33 -0800

    program that prints all possible combinations of single-digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+int n;
+for (n = 48; n < 58; n++)
+if (n != 37)
+{
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}

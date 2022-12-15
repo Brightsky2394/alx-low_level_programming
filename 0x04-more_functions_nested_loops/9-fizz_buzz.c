1e16c4f4e702d45ad5b6031ef54b692c058aaf35
Sikiru Yaya <sikiruyaya1@gmail.com>
2022-12-15 14:12:06 -0800

    program that prints the numbers from 1 to 100

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * main - prints the numbers from 1 to 100, followed by a new line
+ * multiples of three print Fizz instead of the number
+ * and for the multiples of five print Buzz
+ * Return: Always 0
+ */
+int main(void)
+{
+int a;
+a = 1;
+while (a <= 100)
+{
+if (a % 3 == 0 && a % 5 == 0)
+	printf("FizzBuzz");
+else if (a % 3 == 0)
+	printf("Fizz");
+else if (a % 5 == 0)
+	printf("Buzz");
+else
+	printf("%d", a);
+if (i != 100)
+	printf(" ");
+a++;
+}
+printf("\n");
+return (0);
+}

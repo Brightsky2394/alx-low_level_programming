e60830a466dad3b6ab74c48a4e40db395d91562c
Sikiru Yaya <sikiruyaya1@gmail.com>
2022-12-16 08:42:16 -0800

    program that prints the numbers from 1 to 100, followed by a new line

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,20 +7,26 @@
  */
 int main(void)
 {
-int i;
-for (i = 1; i <= 100; i++)
+int h = 1;
+while (h <= 100)
 {
-if (i % 15 == 0)
-	printf("FizzBuzz");
-else if (i % 3 == 0)
-	printf("Fizz");
-else if (i % 5 == 0)
-	printf("Buzz");
-else
-	printf("%d", i);
-if (i < 100)
-	printf(" ");
-}
+if (h % 3 == 0 && h % 5 == 0)
+printf("FizzBuzz ");
+else if (h % 5 == 0)
+{
+if (h == 100)
+{
+printf("Buzz");
 printf("\n");
+}
+else
+printf("Buzz ");
+}
+else if (h % 3 == 0)
+printf("Fizz ");
+else
+printf("%d ", h);
+h++;
+}
 return (0);
 }

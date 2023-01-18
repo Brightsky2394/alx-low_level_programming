da8594b712e91924d63e887bb6e44bb1ee75f315
Sikiru Yaya <sikiruyaya1@gmail.com>
2023-01-18 10:09:21 -0800

    program that performs simple operations

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,29 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - program that performs simple operations
+ * @argv: Pointer of array of pointers containing strings entering main
+ * @argc: Counts the number of parameters that go into main
+ * Return: No element matches -1, if size <=0 -1
+ */
+int main(int argc, char **argv)
+{
+int (*oprt)(int, int);
+if (argc != 4)
+{
+printf("Error\n");
+exit(98);
+}
+oprt = get_op_func(argv[2]);
+if (!oprt)
+{
+printf("Error\n");
+exit(99);
+}
+printf("%d\n", oprt(atoi(argv[1]), atoi(argv[3])));
+return (0);
+}
+

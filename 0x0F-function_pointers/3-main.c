f2c7f8334964cf3fe91900b71406adff9eae99ae
Sikiru Yaya <sikiruyaya1@gmail.com>
2023-01-18 12:19:52 -0800

    program that performs simple operations

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -7,27 +7,20 @@
  * @argc: Counts the number of parameters that go into main
  * Return: No element matches -1, if size <=0 -1
  */
-int main(int __attribute__((__unused__)) argc, char *argv[])
+int main(int argc, char *argv[])
 {
-int num1, num2;
+int (*oprt)(int, int);
 if (argc != 4)
 {
 printf("Error\n");
 exit(98);
 }
-num1 = atoi(argv[1]);
-op = argv[2];
-num2 = atoi(argv[3]);
-if (get_op_func(op) == NULL || op[1] != '\0')
+oprt = get_op_func(argv[2]);
+if (!oprt)
 {
 printf("Error\n");
 exit(99);
 }
-if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
-{
-printf("Error\n");
-exit(100);
-}
-printf("%d\n", get_op_func(op)(num1, num2));
+printf("%d\n", oprt(atoi(argv[1]), atoi(argv[3])));
 return (0);
 }

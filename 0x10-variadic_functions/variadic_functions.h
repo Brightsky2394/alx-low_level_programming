c3c756d1523a64390b8217d88bbca30a880bace4
Sikiru Yaya <sikiruyaya1@gmail.com>
2023-01-19 04:27:31 -0800

    function that returns the sum of all its parameters

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,7 +1,17 @@
 #ifndef FUCTIONS_H
 #define FUNCTIONS_H
 #include <stdarg.h>
-#include <stdio.h>
+/**
+ * struct Ftype - Struct op
+ * @tc: Parameter
+ * @tf: The function associated
+ */
+typedef struct Ftype
+{
+char *tc;
+void (*tf)(va_list);
+} ftype;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);

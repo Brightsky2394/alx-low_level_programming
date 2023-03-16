5dc19a7d19470779e69a6890fd5f4cbfad5232d7
Sikiru Yaya <sikiruyaya1@gmail.com>
2023-03-16 08:42:39 -0700

    function that inserts a new node at a given position.

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,88 +1,46 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
-  * insert_dnodeint_at_index - Inserts a new node at a given position
-  * @h: The head of the doubly linked list
-  * @idx: The index in which insert the new node
-  * @n: The number to insert in the new node
-  *
-  * Return: The address of the new node, or NULL if it failed
-  */
+ * insert_dnodeint_at_index - func to insert node at index
+ * @h: double ptr to the beginning of the list
+ * @idx: index at which to add
+ * @n: data to be added
+ * Return: address of the new node or NULL
+ */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *current = NULL, *new_node = NULL;
-	unsigned int iter_times = 0, length = 0;
+	dlistint_t *new, *old;
+	unsigned int a;
 
 	if (h == NULL)
 		return (NULL);
 
-	if (*h == NULL && idx == 0)
-		return (add_dnodeint(h, n));
-
-	length = dlistint_len(*h);
 	if (idx == 0)
 		return (add_dnodeint(h, n));
-	else if (length == idx)
+
+	old = *h;
+
+	for (a = 0; old != NULL && a < idx; a++)
+		old = old->next;
+
+	if (old == NULL && a == idx)
 		return (add_dnodeint_end(h, n));
 
-	current = *h;
-	while (current != NULL)
+	else if (old != NULL)
 	{
-		if (iter_times == idx)
-		{
-			new_node = create_node(n, current, current->prev);
-			current->prev = new_node;
-			current = new_node;
-			current->prev->next = new_node;
-			return (new_node);
-		}
+		new = malloc(sizeof(dlistint_t));
 
-		current = current->next;
-		++iter_times;
+		if (new == NULL)
+			return (NULL);
+		new->n = n;
+		old->prev->next = new;
+		new->prev = old->prev;
+		old->prev = new;
+		new->next = old;
+
+		return (new);
 	}
-
-	return (current);
-}
-
-/**
-  * dlistint_len - Counts the number of elements in a doubly linked list
-  * @h: The double linked list to count
-  *
-  * Return: Number of elements in the doubly linked list
-  */
-size_t dlistint_len(const dlistint_t *h)
-{
-	int lenght = 0;
-
-	while (h != NULL)
-	{
-		++lenght;
-		h = h->next;
-	}
-
-	return (lenght);
-}
-
-/**
-  * create_node - Create a new node with values
-  * @n: The number of the new node
-  * @next: The next node of the new node
-  * @prev: The previous node of the new node
-  *
-  * Return: The address of the new node created
-  */
-dlistint_t *create_node(unsigned int n, void *next, void *prev)
-{
-	dlistint_t *new_node = NULL;
-
-	new_node = malloc(sizeof(dlistint_t));
-	if (new_node == NULL)
-		return (NULL);
-
-	new_node->n = n;
-	new_node->next = next;
-	new_node->prev = prev;
-	return (new_node);
+	return (NULL);
 }
